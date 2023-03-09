9920a0c6a4252aa465af36bcea076a411abe7ef2
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2023-03-09 20:01:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
+}
+
+const listaTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0
+
+
+for(let i = 0;i < listaTeclas.length; i++) {
+    
+    const tecla = listaTeclas[i]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa')
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+
+    //console.log(idAudio)
+ }
